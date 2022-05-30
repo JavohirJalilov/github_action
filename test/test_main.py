e245dc42c7632fb89a6f07876910ca867d9faa17
Jalilov <javohirjalilov777@gmail.com>
2022-05-30 18:22:04 +0500

    numpy library

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,11 @@
-from unittest import expectedFailure
 import main
-
+import numpy as np 
 def test_main():
     expect = main.a
     output = "Hello world!"
     assert expect == output, "Wrong answer"
+
+def test_main_1():
+    expect = main.x
+    output = np.array([[1, 2, 3], [4, 5, 6]])
+    assert np.array_equal(expect, output), "Wrong answer"
